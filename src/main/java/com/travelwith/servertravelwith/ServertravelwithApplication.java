b1c7f14f86adc67b1c568c0e2c03ad10c0e1acb8
dckx37 <dckx37@motorolasolutions.com>
2018-10-27 13:33:27 +0200

    Initial commit

---
+++ src/main/java/com/travelwith/servertravelwith/ServertravelwithApplication.java
@@ -0,0 +1,12 @@
+package com.travelwith.servertravelwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServertravelwithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServertravelwithApplication.class, args);
+	}
+}
